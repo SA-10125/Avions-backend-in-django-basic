6eb05bc47a2bac1adb6570db66042fe662156a00
Suraj Acharya <surajacharya2005@gmail.com>
2025-07-07 01:23:11 +0530

    Basssiccsss

---
+++ Avions1/home/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.render_simple_homepage,name='home'),
+    path('login',views.login_page,name='login'),
+    path('logout',views.logout_page,name='logout'),
+]
